ec6889698a829f39b4e30f7d0cc040d0b2c15e52
Anand Kumar <anand.kumar@ticketmaster.com>
2022-03-28 23:19:56 +0530

    filter polyfil

---
+++ filter.js
@@ -0,0 +1,9 @@
+Array.prototype.polyfilter = function(callback, context) {
+    const output = [];
+    for(let i = 0; i < this.length; i++) {
+        if(callback.call(context, this[i], i, this)) {
+            output.push(this[i]);
+        }
+    } 
+    return output;
+}
