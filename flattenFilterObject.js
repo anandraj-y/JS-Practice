5a4f31362c07a4da03b3f5fb5823c5ee5c4d3153
Anand Kumar <anand.kumar@ticketmaster.com>
2022-04-09 23:03:56 +0530

    Filter nested object in Javascript | Added input and output

--- flattenFilterObject.js
+++ flattenFilterObject.js
@@ -1,3 +1,37 @@
+/**
+ * Create a function in javascript which will take a
+ * nested object and a filter function as input and return the filtered object.
+ */
+/**
+ *Input:
+const obj = {
+  a: 1,
+  b: {
+    c: "Hello World",
+    d: 2,
+    e: {
+     f: {
+       g: -4,
+      },
+    },
+    h: "Good Night Moon",
+  },
+};
+
+const filter = (s) => typeof s === "string";
+
+Output:
+{
+  b: {
+    c: "Hello World",
+    h: "Good Night Moon",
+  }
+};
+ *
+ *
+ *
+ */
+
 const obj = {
   a: 1,
   b: {
